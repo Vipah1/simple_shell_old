a45dd6e4876e693f715e31d494e1fbb1aef2b47c
Osman <oselibas@gmail.com>
2023-05-22 09:52:14 -0700

    shell.h file updateed with necessary prototypes

--- shell.h
+++ shell.h
@@ -10,8 +10,11 @@
 #include <sys/types.h>
 #include <limits.h>
 #include <signal.h>
+#include <errno.h>
 
 #define BUFSIZE 1024
+#define TRUE 1
+#define FALSE 0
 
 extern char **environ;
 
@@ -23,9 +26,41 @@ char **tokenize(char *s, const char *delimiter);
 /* printing tools */
 int _putchar(char c);
 void _puts(char *str);
+int print(char *str);
 /* string manipulating tools */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
 char *_strdup(char *str);
+char *_strstr(char *haystack, char *needle);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+int _strcmp(char *s1, char *s2);
 
+char *_getenv(const char *name);
+
+/**
+ * struct builtin_t - this structure is for builtin commands
+ * @cmd: this is the command name
+ * @f: this is the correct function to be called
+ */
+typedef struct builtin_t
+{
+  char *cmd;
+  int (*f)(char **, int, char *);
+}builtin_t;
+
+/* utils */
+void free_memory_p(char *);
+void free_memory_pp(char **);
+int cmp(const char *s1, const char *s2);
+int _atoi(char *s);
+int _isalpha(char c);
+
+char *check_cmd_path(char **cmd);
+char *rm_newline(char *cmd);
+/* builtins */
+int exit_cmd(char **, int, char *);
+int env_cmd(char **, int, char *);
+builtin_t is_builtin(char *cmd);
+int (*check_builtins(char **))(char **, int, char *);
 #endif
